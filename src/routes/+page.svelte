2e43c3057607d88889cf61ae85bc219a503c5af5
Jeff Allen <jeffallen89@gmail.com>
2024-06-18 17:58:43 -0400

    some css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,17 +25,24 @@ onMount(() => {
 </script>
 
 
-
+<div id="container">
 
 <div id="panel">
-	<h2>Non-Profit Real Estate</h2>
-	<h3>Toronto, Peel, York</h3>
+	<h1>Non-Profit Real Estate</h1>
+	<h2>Toronto, Peel, York</h2>
     <!-- Content for the left panel -->
     <!-- You can add text, images, or other elements here -->
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+	</p>
 </div>
+
+
 
 <div id="map">
 	
     <!-- Content for the right panel -->
     <!-- You can add text, images, or other elements here -->
+</div>
+
 </div>
