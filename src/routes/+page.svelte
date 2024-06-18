1be52e95bf67ac47f286fd433f93969f27d3029c
Jeff Allen <jeffallen89@gmail.com>
2024-06-18 16:47:17 -0400

    adding map/basemap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,26 @@
 <script>
 
+import { onMount } from "svelte";
+import maplibregl from "maplibre-gl";
 import "../assets/styles.css";
+import cartoBasemap from "../assets/carto-basemap.json";
+
+
+onMount(() => {
+
+	map = new maplibregl.Map({
+        container: "map",
+        style: cartoBasemap, //'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+        center: [-79.0, 44.1], // starting position
+        zoom: 8, // starting zoom;
+        minZoom: 2,
+        maxZoom: 17,
+        projection: "globe",
+        scrollZoom: true,
+        attributionControl: true,
+    });
+
+})
 
 </script>
 
@@ -8,8 +28,8 @@ import "../assets/styles.css";
 
 
 <div id="panel">
-	
-
+	<h2>Non-Profit Real Estate</h2>
+	<h3>Toronto, Peel, York</h3>
     <!-- Content for the left panel -->
     <!-- You can add text, images, or other elements here -->
 </div>
